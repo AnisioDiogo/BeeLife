4f1a43ca2be820d51ac1fb6af9bad01943779797
Anisio Diogo <168247565+AnisioDiogo@users.noreply.github.com>
2025-04-07 18:16:22 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://beelife-private.onrender.com"],
+        "https://beelife-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
