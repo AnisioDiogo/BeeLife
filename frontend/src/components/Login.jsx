64996fcd9084671791d958bf693c777dd4f8f087
Anisio Diogo <168247565+AnisioDiogo@users.noreply.github.com>
2025-04-07 12:44:32 -0300

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
     e.preventDefault(); // Evita que a página recarregue
     try {
       // Faz a requisição de login usando a API
-      const response = await axios.post("http://localhost:8000/api/users/login", formData);
+      const response = await axios.post("https://beelifeapi.onrender.com/api/users/login", formData);
   
       // Salva o token e o username no localStorage
       localStorage.setItem("token", response.data.token);
