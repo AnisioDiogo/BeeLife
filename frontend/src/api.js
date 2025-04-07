a1a1e999af0f364098a11bfbc47906436c8bc72d
Anisio Diogo <168247565+AnisioDiogo@users.noreply.github.com>
2025-04-07 12:32:47 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "https://beelifeapi.onrender.com/api";
 
 // Função para registrar usuário
 export const registerUser = async (username, password) => {
